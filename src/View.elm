2a2ca3c1a3ff85390e8d3d9528ca33c2861be206
Fabian Kirchner <kirchner@posteo.de>
2017-05-30 23:22:13 +0200

    style tables, wip

--- src/View.elm
+++ src/View.elm
@@ -1,6 +1,7 @@
 module View exposing (css, view)
 
 import Css exposing (..)
+import Css.Elements
 import Css.Namespace exposing (namespace)
 import Dict exposing (Dict)
 import Editor
@@ -17,6 +18,7 @@ import Html exposing (Html)
 import Html.Attributes as Html
 import Html.CssHelpers exposing (withNamespace)
 import Html.Events as Events
+import Math.Vector2 exposing (..)
 import Svg exposing (Svg)
 import Tools.Absolute as Absolute
 import Tools.Relative as Relative
@@ -119,86 +121,107 @@ viewPointList variables store =
     let
         styles =
             Css.asPairs >> Html.style
+
+        rem =
+            Css.rem
     in
     Html.div
-        [ styles
-            [ displayFlex
-            , flexFlow1 column
-            , color (hex base0)
-            , backgroundColor (hex base03)
-            ]
-        ]
-        [ Html.div
-            []
-            [ Html.text "point list" ]
-        , Dict.toList store
-            |> List.map (viewPointEntry variables)
-            |> Html.div
+        [ class [ PointList ] ]
+        [ Html.table
+            [ styles [ borderCollapse collapse ] ]
+            (Html.tr
                 [ styles
-                    [ displayFlex
-                    , flexFlow1 column
-                    ]
+                    [ borderBottom3 (px 1) solid (hex base02) ]
                 ]
+                [ Html.th
+                    [ class [ HeaderCell ] ]
+                    [ Html.text "#" ]
+                , Html.th
+                    [ class [ HeaderCell ]
+                    , styles
+                        [ width (rem 3) ]
+                    ]
+                    [ Html.text "x" ]
+                , Html.th
+                    [ class [ HeaderCell ]
+                    , styles
+                        [ width (rem 3) ]
+                    ]
+                    [ Html.text "y" ]
+                , Html.th [ class [ HeaderCell ] ] []
+                , Html.th [ class [ HeaderCell ] ] []
+                , Html.th [ class [ HeaderCell ] ] []
+                ]
+                :: (Dict.toList store |> List.map (viewPointEntry variables store))
+            )
         ]
 
 
-viewPointEntry : Dict String E -> ( Id, Point ) -> Html Msg
-viewPointEntry variables ( id, point ) =
+viewPointEntry : Dict String E -> PointStore -> ( Id, Point ) -> Html Msg
+viewPointEntry variables store ( id, point ) =
     let
         styles =
             Css.asPairs >> Html.style
+
+        v =
+            Types.position store variables point
+
+        x =
+            v
+                |> Maybe.map getX
+                |> Maybe.map toString
+                |> Maybe.withDefault ""
+
+        y =
+            v
+                |> Maybe.map getY
+                |> Maybe.map toString
+                |> Maybe.withDefault ""
+
+        icon name callback =
+            Html.div
+                [ class [ IconButton ] ]
+                [ Html.i
+                    [ Html.class "material-icons"
+                    , Events.onClick callback
+                    , class [ Icon ]
+                    ]
+                    [ Html.text name ]
+                ]
     in
-    Html.div
-        [ styles
-            [ displayFlex
-            , flexFlow1 row
+    Html.tr [ class [ ContentRow ] ]
+        [ Html.td
+            [ class [ ContentCell ] ]
+            [ Html.text (toString id) ]
+        , Html.td
+            [ class [ ContentCell ] ]
+            [ Html.text x ]
+        , Html.td [ class [ ContentCell ] ]
+            [ Html.text y ]
+        , Html.td
+            [ class [ ContentCell ]
+            , styles
+                [ textAlign left
+                , paddingLeft (Css.rem 1.5)
+                , paddingRight (Css.rem 1.5)
+                ]
             ]
-        ]
-        [ Html.div []
-            [ Html.text (toString id ++ ": " ++ printPoint variables point) ]
-        , Html.div
-            [ Events.onClick (SelectPoint id)
-            , class [ Button ]
-            ]
-            [ Html.text "edit" ]
-        , Html.div
-            [ Events.onClick (DeletePoint id)
-            , class [ Button ]
-            ]
-            [ Html.text "delete" ]
+            [ Html.text (printPoint variables point) ]
+        , Html.td [ class [ ContentCell ] ]
+            [ icon "edit" (SelectPoint id) ]
+        , Html.td [ class [ ContentCell ] ]
+            [ icon "delete" (DeletePoint id) ]
         ]
 
 
 printPoint : Dict String E -> Point -> String
 printPoint variables point =
     case point of
-        Types.Absolute x y ->
-            String.concat
-                [ "("
-                , Expr.print x
-                , ", "
-                , Expr.print y
-                , ") = ("
-                , toString (compute variables x)
-                , ", "
-                , toString (compute variables y)
-                , ")"
-                ]
+        Types.Absolute _ _ ->
+            "absolute"
 
-        Types.Relative anchorId p q ->
-            String.concat
-                [ "from "
-                , toString anchorId
-                , ": ("
-                , Expr.print p
-                , ", "
-                , Expr.print q
-                , ") = ("
-                , toString (compute variables p)
-                , ", "
-                , toString (compute variables q)
-                , ")"
-                ]
+        Types.Relative _ _ _ ->
+            "relative"
 
         _ ->
             toString point
@@ -213,87 +236,83 @@ viewVariableList variables newName newValue =
     let
         styles =
             Css.asPairs >> Html.style
+
+        icon name callback =
+            Html.div
+                [ class [ IconButton ] ]
+                [ Html.i
+                    [ Html.class "material-icons"
+                    , Events.onClick callback
+                    , class [ Icon ]
+                    ]
+                    [ Html.text name ]
+                ]
     in
     Html.div
-        [ styles
-            [ displayFlex
-            , flexFlow1 column
-            , color (hex base0)
-            , backgroundColor (hex base03)
-            ]
-        ]
-        [ Html.div
-            []
-            [ Html.text "variables" ]
-        , Dict.toList variables
-            |> List.map (viewVariable variables)
-            |> Html.div
-                [ styles
-                    [ displayFlex
-                    , flexFlow1 column
-                    ]
-                ]
-        , Html.div
-            [ styles
-                [ displayFlex
-                , flexFlow1 row
-                ]
-            ]
-            [ Html.div []
-                [ Html.text "name:" ]
-            , Html.input
-                [ Events.onInput NameUpdated
-                , styles
-                    [ case newName of
-                        Nothing ->
-                            color (hex red)
+        [ class [ VariableList ] ]
+        [ Html.table
+            [ styles [ borderCollapse collapse ] ]
+            (Dict.toList variables |> List.map (viewVariable variables))
+        , Html.tr [ class [ ContentRow ] ]
+            [ Html.td
+                [ class [ ContentCell ] ]
+                [ Html.input
+                    [ Events.onInput NameUpdated
+                    , Html.placeholder "name"
+                    , styles
+                        [ case newName of
+                            Nothing ->
+                                color (hex red)
 
-                        Just _ ->
-                            color (hex base0)
-                    , backgroundColor (hex base03)
-                    , borderColor transparent
-                    , fontFamily monospace
-                    , fontSize (Css.rem 1)
-                    , lineHeight (Css.rem 1)
-                    , width (Css.rem 4.8)
-                    , backgroundColor transparent
-                    , focus
-                        [ outline none
-                        , borderColor (hex base02)
+                            Just _ ->
+                                color (hex base0)
+                        , backgroundColor (hex base03)
+                        , borderColor transparent
+                        , border zero
+                        , fontFamily monospace
+                        , fontSize (Css.rem 1)
+                        , lineHeight (Css.rem 1)
+                        , width (Css.rem 6)
+                        , backgroundColor transparent
+                        , focus
+                            [ outline none
+                            , borderColor (hex base02)
+                            ]
                         ]
                     ]
+                    []
                 ]
-                []
-            , Html.div []
-                [ Html.text "value:" ]
-            , Html.input
-                [ Events.onInput ValueUpdated
-                , styles
-                    [ case newValue of
-                        Nothing ->
-                            color (hex red)
+            , Html.td
+                [ class [ ContentCell ] ]
+                [ Html.input
+                    [ Events.onInput ValueUpdated
+                    , Html.placeholder "value"
+                    , styles
+                        [ case newValue of
+                            Nothing ->
+                                color (hex red)
 
-                        Just _ ->
-                            color (hex base0)
-                    , backgroundColor (hex base03)
-                    , borderColor transparent
-                    , fontFamily monospace
-                    , fontSize (Css.rem 1)
-                    , lineHeight (Css.rem 1)
-                    , width (Css.rem 4.8)
-                    , backgroundColor transparent
-                    , focus
-                        [ outline none
-                        , borderColor (hex base02)
+                            Just _ ->
+                                color (hex base0)
+                        , backgroundColor (hex base03)
+                        , borderColor transparent
+                        , border zero
+                        , fontFamily monospace
+                        , fontSize (Css.rem 1)
+                        , lineHeight (Css.rem 1)
+                        , width (Css.rem 10)
+                        , backgroundColor transparent
+                        , focus
+                            [ outline none
+                            , borderColor (hex base02)
+                            ]
                         ]
                     ]
+                    []
                 ]
-                []
-            , Html.div
-                [ class [ Button ]
-                , Events.onClick AddVariable
-                ]
-                [ Html.text "add" ]
+            , Html.td
+                [ class [ ContentCell ] ]
+                [ icon "add" AddVariable ]
             ]
         ]
 
@@ -391,6 +410,13 @@ type Class
     | ButtonWrapper
     | Button
     | Tooltip
+    | PointList
+    | VariableList
+    | HeaderCell
+    | ContentRow
+    | ContentCell
+    | IconButton
+    | Icon
 
 
 { id, class, classList } =
@@ -399,7 +425,9 @@ type Class
 
 css =
     (stylesheet << namespace "toolbar")
-        [ Css.class Main
+        [ Css.Elements.body
+            [ margin zero ]
+        , Css.class Main
             [ displayFlex
             , flexFlow1 row
             , property "pointer-events" "auto"
@@ -451,5 +479,60 @@ css =
             , backgroundColor (hex base2)
             , fontSize smaller
             , borderRadius (px 2)
+            ]
+        , Css.class PointList
+            [ color (hex base0)
+            , backgroundColor (hex base2)
+            , property "pointer-events" "auto"
+            , fontFamily monospace
+            , fontSize (Css.rem 1)
+            , lineHeight (Css.rem 1)
+            ]
+        , Css.class VariableList
+            [ color (hex base0)
+            , backgroundColor (hex base2)
+            , property "pointer-events" "auto"
+            , fontFamily monospace
+            , fontSize (Css.rem 1)
+            , lineHeight (Css.rem 1)
+            ]
+        , Css.class HeaderCell
+            [ paddingLeft (Css.rem 0.3)
+            , paddingRight (Css.rem 0.3)
+            , paddingTop (Css.rem 0.1)
+            , paddingBottom (Css.rem 0.2)
+            , borderBottom3 (px 1) solid (hex base02)
+            , textAlign right
+            ]
+        , Css.class ContentRow
+            [ hover
+                [ backgroundColor (hex base3) ]
+            ]
+        , Css.class ContentCell
+            [ paddingLeft (Css.rem 0.3)
+            , paddingRight (Css.rem 0.3)
+            , paddingTop (Css.rem 0.1)
+            , paddingBottom (Css.rem 0.1)
+            , textAlign right
+            ]
+        , Css.class IconButton
+            [ width (Css.rem 1)
+            , height (Css.rem 1)
+            , borderRadius (pct 50)
+            , color (hex base0)
+            , backgroundColor transparent
+            , cursor pointer
+            , hover
+                [ backgroundColor (hex base02)
+                ]
+            , Css.position Css.relative
+            ]
+        , Css.class Icon
+            [ important (Css.fontSize (Css.rem 0.6))
+            , important (Css.lineHeight (Css.rem 0.6))
+            , Css.position Css.absolute
+            , Css.top (Css.pct 50)
+            , Css.left (Css.pct 50)
+            , Css.transform (Css.translate2 (Css.pct -50) (Css.pct -50))
             ]
         ]
