361eb818aee5490369ecf4ca4c894fbb0aa41c73
Fabian Kirchner <kirchner@posteo.de>
2017-06-23 21:48:16 +0200

    compute positions

--- src/Piece.elm
+++ src/Piece.elm
@@ -1,16 +1,25 @@
 module Piece exposing (Piece)
 
 import Dict exposing (Dict)
+import Expr exposing (..)
 import Types exposing (..)
 
 
 type Piece
-    = Piece {}
+    = Piece { points : List Id }
 
 
-fromList : PointStore -> Dict String Id -> List Id -> Maybe Piece
+fromList : PointStore -> Dict String E -> List Id -> Maybe Piece
 fromList store variables points =
-    Debug.crash "implement fromList"
+    let
+        positions =
+            points
+                |> List.filterMap (positionById store variables)
+    in
+    if List.length positions < List.length points then
+        Nothing
+    else
+        Debug.crash "implement fromList"
 
 
 toList : Piece -> List Id
@@ -18,11 +27,11 @@ toList piece =
     Debug.crash "implement toList"
 
 
-insertAfter : PointStore -> Dict String Id -> Id -> Id -> Piece -> Piece
+insertAfter : PointStore -> Dict String E -> Id -> Id -> Piece -> Piece
 insertAfter store variables new reference piece =
     Debug.crash "implement insertAfter"
 
 
-insertBefore : PointStore -> Dict String Id -> Id -> Id -> Piece -> Piece
+insertBefore : PointStore -> Dict String E -> Id -> Id -> Piece -> Piece
 insertBefore store variables new reference piece =
     Debug.crash "implement insertBefore"
