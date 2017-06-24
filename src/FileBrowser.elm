d06d95a24ce8e8c35ee177da7a8d9cee16b1fc5c
Fabian Kirchner <kirchner@posteo.de>
2017-06-24 17:24:52 +0200

    fix link

--- src/FileBrowser.elm
+++ src/FileBrowser.elm
@@ -46,7 +46,7 @@ view callbacks data =
               [ class [ FileBrowserFileLinkWrapper ]
               ]
               [ fileLink (github "demo-demo.json") "demo-demo"
-              , fileLink (github "basic_bodices.json") "basic bodices"
+              , fileLink (github "basic_bodice.json") "basic bodice"
               ]
             ]
           , [ iconBig "close" <|
